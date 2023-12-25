f2f07f830e164451c86a681e40ac91dce703e079
savasafsar <savasafsar3443@gmail.com>
2023-12-25 15:39:17 +0300

    subjectscreenadd

--- app/src/main/java/com/example/studysmart/MainActivity.kt
+++ app/src/main/java/com/example/studysmart/MainActivity.kt
@@ -12,9 +12,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.studysmart.domain.model.Session
 import com.example.studysmart.domain.model.Subject
+import com.example.studysmart.domain.model.Task
 import com.example.studysmart.ui.presentation.dashboard.DashboardScreen
 import com.example.studysmart.ui.presentation.dashboard.DashboardScreenTopBar
 import com.example.studysmart.ui.presentation.session.SessionScreen
+import com.example.studysmart.ui.presentation.subject.SubjectScreen
 import com.example.studysmart.ui.presentation.task.TaskScreen
 import com.example.studysmart.ui.theme.StudySmartTheme
 
@@ -23,79 +25,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StudySmartTheme {
-              SessionScreen()
+              SubjectScreen()
             }
         }
     }
-}val subjects = listOf(
-    Subject(
-        name = "English",
-        goalHours = 10f,
-        colors = Subject.subjectCardColors[0],
-        subjectId = 0
-    ),
-    Subject(
-        name = "Physics",
-        goalHours = 10f,
-        colors = Subject.subjectCardColors[1],
-        subjectId = 0
-    ),
-    Subject(
-        name = "Maths",
-        goalHours = 10f,
-        colors = Subject.subjectCardColors[2],
-        subjectId = 0
-    ),
-    Subject(
-        name = "Geology",
-        goalHours = 10f,
-        colors = Subject.subjectCardColors[3],
-        subjectId = 0
-    ),
-    Subject(
-        name = "Fine Arts",
-        goalHours = 10f,
-        colors = Subject.subjectCardColors[4],
-        subjectId = 0
-    ),
+}
+val subjects = listOf(
+    Subject("English", 10f, Subject.subjectCardColors[0],0),
+    Subject("Physics", 10f, Subject.subjectCardColors[1],0),
+    Subject("Maths", 10f, Subject.subjectCardColors[2],0),
+    Subject("Geology", 10f, Subject.subjectCardColors[3],0),
+    Subject("Fine Arts", 10f, Subject.subjectCardColors[4],0),
 )
-val sessions = listOf(
-    Session(
-        relatedToSubject = "English",
-        date = 0L,
-        duration = 2,
-        sessionSubjectId = 0,
-        sessionId = 0
-    ),
-    Session(
-        relatedToSubject = "English",
-        date = 0L,
-        duration = 2,
-        sessionSubjectId = 0,
-        sessionId = 0
-    ),
-    Session(
-        relatedToSubject = "Physics",
-        date = 0L,
-        duration = 2,
-        sessionSubjectId = 0,
-        sessionId = 0
-    ),
-    Session(
-        relatedToSubject = "Maths",
-        date = 0L,
-        duration = 2,
-        sessionSubjectId = 0,
-        sessionId = 0
-    ),
-    Session(
-        relatedToSubject = "English",
-        date = 0L,
-        duration = 2,
-        sessionSubjectId = 0,
-        sessionId = 0
+
+val tasks = listOf(
+    Task("Prepare notes","",0L,0,"",false,0,1),
+    Task("Do Homework","",0L,1,"",true,0,1),
+    Task("Go Coaching","",0L,2,"",false,0,1),
+    Task("Assignment","",0L,0,"",false,0,1),
+    Task("Write Poew","",0L,0,"",true,0,1),
+
     )
+
+val sessions = listOf(
+    Session(relatedToSubject = "English", date = 0L, duration = 2, sessionSubjectId =0, sessionId =0  ),
+    Session(relatedToSubject = "Physics", date = 0L, duration = 2, sessionSubjectId =0, sessionId =0  ),
+    Session(relatedToSubject = "Maths", date = 0L, duration = 2, sessionSubjectId =0, sessionId =0  ),
+    Session(relatedToSubject = "English", date = 0L, duration = 2, sessionSubjectId =0, sessionId =0  ),
 )
-
-
-
--- app/src/main/java/com/example/studysmart/ui/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/studysmart/ui/presentation/dashboard/DashboardScreen.kt
@@ -45,6 +45,9 @@ import com.example.studysmart.R
 import com.example.studysmart.domain.model.Session
 import com.example.studysmart.domain.model.Subject
 import com.example.studysmart.domain.model.Task
+import com.example.studysmart.sessions
+import com.example.studysmart.subjects
+import com.example.studysmart.tasks
 import com.example.studysmart.ui.presentation.components.AddSubjectDialog
 import com.example.studysmart.ui.presentation.components.CountCard
 import com.example.studysmart.ui.presentation.components.DeleteDialog
@@ -55,27 +58,7 @@ import com.example.studysmart.ui.presentation.components.tasksList
 @Preview
 @Composable
 fun DashboardScreen() {
-    val subjects = listOf(
-        Subject("English", 10f, Subject.subjectCardColors[0],0),
-        Subject("Physics", 10f, Subject.subjectCardColors[1],0),
-        Subject("Maths", 10f, Subject.subjectCardColors[2],0),
-        Subject("Geology", 10f, Subject.subjectCardColors[3],0),
-        Subject("Fine Arts", 10f, Subject.subjectCardColors[4],0),
-    )
-    val tasks = listOf(
-        Task("Prepare notes","",0L,0,"",false,0,1),
-        Task("Do Homework","",0L,1,"",true,0,1),
-        Task("Go Coaching","",0L,2,"",false,0,1),
-        Task("Assignment","",0L,0,"",false,0,1),
-        Task("Write Poew","",0L,0,"",true,0,1),
 
-    )
-    val sessions = listOf(
-        Session(relatedToSubject = "English", date = 0L, duration = 2, sessionSubjectId =0, sessionId =0  ),
-        Session(relatedToSubject = "Physics", date = 0L, duration = 2, sessionSubjectId =0, sessionId =0  ),
-        Session(relatedToSubject = "Maths", date = 0L, duration = 2, sessionSubjectId =0, sessionId =0  ),
-        Session(relatedToSubject = "English", date = 0L, duration = 2, sessionSubjectId =0, sessionId =0  ),
-    )
     var isAddSubjectDialogOpen by rememberSaveable { mutableStateOf(false) }
     var isDeleteSessionDialogOpen by rememberSaveable { mutableStateOf(false) }
     var subjectName by remember { mutableStateOf("") }
